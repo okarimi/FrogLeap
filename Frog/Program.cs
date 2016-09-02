c73d97d586a04d4ada6c54cfd7bf421be5469f40
Omid Karimi <okarimi@yahoo.com>
2016-09-02 12:22:54 -0700

    One step or two step jumps only.
    Frog can make a one step or to two step jumps. find out with how many
    combination of leaps it can cover a distance.

---
+++ Frog/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Frog
+{
+    public static Int64 NumberOfWays(int n)
+    {
+        // Maximum possible jumps (blue only) for one full traversal.
+        int maxJumps = n / 2;
+
+        // Count of combinations for red only traversal.
+        long count = 1;
+
+        try {
+            // Add all possible combinations of jump-step 
+            for (int jumps = 1; jumps <= maxJumps; jumps++)
+            {
+                // Remaining steps (red) for current jumps.
+                int steps = n - 2 * jumps;
+
+                // For any number of jumps, compute total combination of jump-steps
+                checked
+                {
+                    // Total ordered reds.
+                    Int64 s = factorial(steps);
+
+                    // Total ordered blues.
+                    Int64 j = factorial(jumps);
+
+                    // Total ordered combined.
+                    Int64 sj = factorial(steps + jumps);
+
+                    // Total combined red-blue for unordered red-blue. 
+                    count += sj / (s * j);
+                }
+            }
+        }
+        catch(OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return count;
+    }
+
+    public static Int64 factorial(int n)
+    {
+        checked
+        {
+            Int64 fact = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter a number");
+        string s = Console.ReadLine();
+        Console.WriteLine("The answer is: " + NumberOfWays(int.Parse(s)));
+        Console.Read();
+    }
+}
